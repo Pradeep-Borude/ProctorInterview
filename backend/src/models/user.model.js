259258a3b9bb2d57275aedcd9a9ee426d51cc26f
procoder406 <pradeepborude406@gmail.com>
2025-12-19 17:37:02 +0530

    minor change in user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        required: true,
+        enum: ['interviewer', 'candidate'],
         default: "candidate",
 
     },
